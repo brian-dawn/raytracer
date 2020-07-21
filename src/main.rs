265382282b020babf43c582fa760652dc4ffa77a
Brian Dawn <brian.t.dawn@gmail.com>
2020-07-20 21:31:12 -0500

    Add section 5 (naive spheres).

--- src/main.rs
+++ src/main.rs
@@ -14,25 +14,37 @@ fn write_color(pixel: &Color) {
     println!("{} {} {}", ir, ig, ib);
 }
 
-fn ray_color(ray: &Ray) -> Color {
-    //
-    let unit_direction = ray.direction.unit();
+fn hit_sphere(center: &Point3, radius: f64, r: &Ray) -> bool {
+    let oc = r.origin - *center;
+    let a = r.direction.dot(&r.direction);
+    let b = 2.0 * oc.dot(&r.direction);
+    let c = oc.dot(&oc) - radius * radius;
+    let discriminant = b * b - 4.0 * a * c;
+
+    discriminant > 0.0
+}
+
+fn ray_color(r: &Ray) -> Color {
+    if hit_sphere(&Point3::new(0.0, 0.0, -1.0), 0.5, &r) {
+        return Color::new(1.0, 0.0, 0.0);
+    }
+    let unit_direction = r.direction.unit();
     let t = 0.5 * (unit_direction.y + 1.0);
     (1.0 - t) * Color::ones() + t * Color::new(0.5, 0.7, 1.0)
 }
 
 fn main() {
-    const aspect_ratio: f64 = 16.0 / 9.0;
+    const ASPECT_RATIO: f64 = 16.0 / 9.0;
 
     // Image
 
     let image_width = 256;
-    let image_height = (image_width as f64 / aspect_ratio) as i32;
+    let image_height = (image_width as f64 / ASPECT_RATIO) as i32;
 
     // Camera
 
     let viewport_height = 2.0;
-    let viewport_width = aspect_ratio * viewport_height;
+    let viewport_width = ASPECT_RATIO * viewport_height;
     let focal_length = 1.0;
 
     let origin = Point3::zero();
