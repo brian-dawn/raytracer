6994c4c4f31358408ef6c02e2013e997b61d5a40
Brian Dawn <brian.t.dawn@gmail.com>
2020-07-25 08:47:10 -0500

    Add look at and look from on camera (11.2).

--- src/camera.rs
+++ src/camera.rs
@@ -10,19 +10,26 @@ pub struct Camera {
 
 impl Camera {
     /// vfov is vertical field of view in degrees.
-    pub fn new(vfov: f64, aspect_ratio: f64) -> Camera {
+    pub fn new(
+        lookfrom: Point3,
+        lookat: Point3,
+        vup: Vec3,
+        vfov: f64,
+        aspect_ratio: f64,
+    ) -> Camera {
         let theta = vfov.to_radians();
         let h = (theta / 2.0).tan();
         let viewport_height = 2.0 * h;
         let viewport_width = aspect_ratio * viewport_height;
 
-        let focal_length = 1.0;
+        let w = (lookfrom - lookat).unit();
+        let u = vup.cross(&w).unit();
+        let v = w.cross(&u);
 
-        let origin = Point3::zero();
-        let horizontal = Vec3::new(viewport_width, 0.0, 0.0);
-        let vertical = Vec3::new(0.0, viewport_height, 0.0);
-        let lower_left_corner =
-            origin - horizontal / 2.0 - vertical / 2.0 - Vec3::new(0.0, 0.0, focal_length);
+        let origin = lookfrom;
+        let horizontal = viewport_width * u;
+        let vertical = viewport_height * v;
+        let lower_left_corner = origin - horizontal / 2.0 - vertical / 2.0 - w;
 
         Camera {
             origin,
@@ -32,10 +39,10 @@ impl Camera {
         }
     }
 
-    pub fn get_ray(&self, u: f64, v: f64) -> Ray {
+    pub fn get_ray(&self, s: f64, t: f64) -> Ray {
         Ray::new(
             self.origin,
-            self.lower_left_corner + u * self.horizontal + v * self.vertical - self.origin,
+            self.lower_left_corner + s * self.horizontal + t * self.vertical - self.origin,
         )
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -68,28 +68,59 @@ fn main() {
 
     let mut world = HittableList::new();
 
-    let material_left = materials::lambertian::Lambertian {
-        albedo: Color::new(0.0, 0.0, 1.0),
+    let material_ground = materials::lambertian::Lambertian {
+        albedo: Color::new(0.8, 0.8, 0.0),
     };
 
-    let material_right = materials::lambertian::Lambertian {
-        albedo: Color::new(1.0, 0.0, 0.0),
+    let material_center = materials::lambertian::Lambertian {
+        albedo: Color::new(0.1, 0.2, 0.5),
+    };
+
+    let material_left = materials::dielectric::Dielectric { ref_idx: 1.5 };
+
+    let material_right = materials::metal::Metal {
+        albedo: Color::new(0.8, 0.6, 0.2),
+        fuzz: 0.0,
     };
 
     world.add(Box::new(Sphere::new(
-        Point3::new(-r, 0.0, -1.0),
-        r,
+        Point3::new(0.0, -100.5, -1.0),
+        100.0,
+        Some(Rc::new(material_ground)),
+    )));
+
+    world.add(Box::new(Sphere::new(
+        Point3::new(0.0, 0.0, -1.0),
+        0.5,
+        Some(Rc::new(material_center)),
+    )));
+
+    world.add(Box::new(Sphere::new(
+        Point3::new(-1.0, 0.0, -1.0),
+        0.5,
         Some(Rc::new(material_left)),
     )));
 
     world.add(Box::new(Sphere::new(
-        Point3::new(r, 0.0, -1.0),
-        r,
+        Point3::new(-1.0, 0.0, -1.0),
+        -0.45,
+        Some(Rc::new(material_left)),
+    )));
+
+    world.add(Box::new(Sphere::new(
+        Point3::new(1.0, 0.0, -1.0),
+        0.5,
         Some(Rc::new(material_right)),
     )));
 
     // Camera
-    let cam = Camera::new(90.0, ASPECT_RATIO);
+    let cam = Camera::new(
+        Point3::new(-2.0, 2.0, 1.0),
+        Point3::new(0.0, 0.0, -1.0),
+        Vec3::new(0.0, 1.0, 0.0),
+        20.0,
+        ASPECT_RATIO,
+    );
 
     // Render
     println!("P3\n{} {}\n255", image_width, image_height);
